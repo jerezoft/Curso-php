a9e3fb2c4294d1372fec9a99971ec12ef0cd977b
juan david <anarkigotic@gmail.com>
2017-02-05 09:19:13 -0500

    Programacion orientada a objetos

--- index.php
+++ index.php
@@ -1,46 +1,20 @@
-<?php
-class Loteria
-{
-    public $intentos;
-    public $numero;
-    public $resultado=false;
+<?php 
+class Persona{
+    public $nombre = "carlos";
 
-    public function __construct($intentos,$numero)
-    {
-        $this->intentos = $intentos;
-        $this->numero = $numero;
+    public  function Saludar($nombre){
+        $this->nombre = $nombre;
+        echo "Hola ".$this->nombre;
     }
 
-    public function sorteo(){
-        $minimo = $this->numero/2;
-        $maximo = $this->numero*2;
-        $valorsorteo = rand($minimo,$maximo);
+}
 
-        for($i=0;$i<$this->intentos;$i++){
-            $valorsorteo = rand($minimo,$maximo);
-            self::jugar($valorsorteo);
-            if($this->resultado){
-                $this->intentos = $i;
-                break;
-            }
-        }
-
+class Persononita extends Persona{
+    public function __construct(){
+            Persona::Saludar("juan");
     }
-    private function jugar($valor){
-    if($this->numero == $valor){
-            echo "Felisidades ganaste<br>";
-                $this->resultado = true;
-    }else{}
-        echo "lo siento perdiste<br>";
-            ;
-   }
+    
+}
 
-public function __destruct(){
-    if($this->resultado ==true){
-    echo  "Hola ganaste ne el intento ".$this->intentos;}else{
-        echo "Perdiste en todos los intentos";
-    }
-}}
-$juego = new Loteria(5,10);
-$juego->sorteo();
+$a = new Persononita();
 ?>
